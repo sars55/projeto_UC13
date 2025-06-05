7c0f5e8c132cff4cfdbb44eb4e3fa70e1b63be5c
Sara Vanessa Santos Paiva <sara.vspaiva@senacsp.edu.com.br>
2025-06-04 22:13:56 -0300

    Extensão Mysql2 e Conexão com o banco de dados

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
 const app = express();
+const mysql = require('mysql2');
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port:3306,
+    database: 'ecommerce_pc'
+});
+
+conexao.connect(function(erro){
+    if(erro) {
+        console.error(' Erro ao conectar ao banco de dados:',erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelicida com sucesso!');
+});
 
 app.get("/" , function (req,res){ 
-    res.write("Hello Word");
+    res.write("Hello fine");
     res.end();
 });
 
